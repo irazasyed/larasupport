f0de3e870742916456a468720368eb2393470be4
Syed I.R <syed@lukonet.com>
2015-05-25 04:57:58 +0530

    Add Vendor Publish Command.

---
+++ src/Providers/ArtisanServiceProvider.php
@@ -0,0 +1,50 @@
+<?php namespace Irazasyed\Larasupport\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Irazasyed\Larasupport\Console\VendorPublishCommand;
+
+class ArtisanServiceProvider extends ServiceProvider {
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerVendorPublishCommand();
+
+        $this->commands(['command.vendor.publish']);
+    }
+
+    /**
+     * Register the command.
+     *
+     * @return void
+     */
+    protected function registerVendorPublishCommand()
+    {
+        $this->app->singleton('command.vendor.publish', function($app)
+        {
+            return new VendorPublishCommand($app['files']);
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['command.vendor.publish'];
+    }
+
+}
