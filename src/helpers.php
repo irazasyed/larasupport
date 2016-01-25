a5744235c1dd4686a9640a9019e8da799710195b
Syed Irfaq R <irazasyed@users.noreply.github.com>
2016-01-25 18:07:18 +0530

    Add Elixir Helper.

--- src/helpers.php
+++ src/helpers.php
@@ -59,3 +59,30 @@ if ( ! function_exists('public_path'))
         return env('PUBLIC_PATH', base_path('public')).($path ? '/'.$path : $path);
     }
 }
+
+if (! function_exists('elixir'))
+{
+    /**
+     * Get the path to a versioned Elixir file.
+     *
+     * @param  string $file
+     *
+     * @return string
+     *
+     * @throws \InvalidArgumentException
+     */
+    function elixir($file)
+    {
+        static $manifest = null;
+        
+        if (is_null($manifest)) {
+            $manifest = json_decode(file_get_contents(public_path('build/rev-manifest.json')), true);
+        }
+        
+        if (isset($manifest[$file])) {
+            return '/build/'.$manifest[$file];
+        }
+        
+        throw new InvalidArgumentException("File {$file} not defined in asset manifest.");
+    }
+}
